c1bf62d29d6ac0ef90f4fe7019c7a09b1e33228b
Han Zhu <zhuhan7737@gmail.com>
2017-01-21 11:08:03 -0500

    survey now only runs once when the app starts for the first time

--- app/src/main/java/apackage/pennapps/MainActivity.java
+++ app/src/main/java/apackage/pennapps/MainActivity.java
@@ -1,7 +1,9 @@
 package apackage.pennapps;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -11,6 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE).getBoolean("isfirstrun", true);
+        if (isFirstRun) {
+            getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit().putBoolean("isfirstrun", false).commit();
+        } else {
+            Intent intent = new Intent(this, Go.class);
+            startActivity(intent);
+        }
     }
 
     public void toGo(View view) {
